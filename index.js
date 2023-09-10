5a17bfb0c424edfcd30944d0864377438c36d625
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-10 23:47:47 +0100

    comments index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const Calculate = {
-    factorial: (number) => {
-        if(number === 0) {
+const Calculate = { // create a Calculate object
+    factorial: (number) => { // create a factorial method
+        if(number === 0) { 
             return 1
         }
-        let result = 1;
-        for(let i = 1; i <= number; i++) {
-            result *= i;
+        let result = 1; // initialize result to 1
+        for(let i = 1; i <= number; i++) { // loop through the numbers from 1 to number
+            result *= i; // multiply result by i
         }
-        return result;
+        return result; // return result
      }
 };
 
 
-module.exports = Calculate;
+module.exports = Calculate; // export the Calculate object
 
 
