a2c94063fb6446dc3a68d864beef66a3dfc95050
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-10 23:47:10 +0100

    comments test

--- test/index-test.js
+++ test/index-test.js
@@ -1,23 +1,23 @@
-var assert = require("assert");
-var Calculate =  require('../index.js')
+var assert = require("assert"); // node.js core module called
+var Calculate =  require('../index.js') // require the Calculate class from index.js
 
-describe('Calculate', () => {
-  describe('.factorial', () => {
-    it('returns 120 when given the number 5', () => {
+describe('Calculate', () => { // describe the Calculate class
+  describe('.factorial', () => { // describe the factorial method
+    it('returns 120 when given the number 5', () => { // it describes the test
         //setup
-        const expected = 120;
+        const expected = 120; // expected value
         //exercise
-        const actual = Calculate.factorial(5)
+        const actual = Calculate.factorial(5) // actual value
         //verify
-        assert.strictEqual(actual, expected);
+        assert.strictEqual(actual, expected); // assert that actual === expected    
   });
-    it('returns Error when given number is 0', () => {
+    it('returns Error when given number is 0', () => { // it describes the test
         //setup
-        const expected = 1
+        const expected = 1 // expected value
         //exercise
-        const actual = Calculate.factorial(0);
+        const actual = Calculate.factorial(0); // actual value
         //verify
-        assert.strictEqual(expected, actual)
+        assert.strictEqual(expected, actual) // assert that actual === expected
     });
   });
 });
